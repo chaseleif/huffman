f58c4d0c290cd2d8296c4561913d9d3e1131a9a9
chaseleif <chaseleif@icloud.com>
2020-11-11 15:51:11 -0600

    readd the rm command

--- gentreepng.sh
+++ gentreepng.sh
@@ -3,5 +3,5 @@
 ./hfc -b $1 _temptree.nfo
 python makedot.py _temptree.nfo
 dot -Tpng _temptree.dot -o $2.png
-#rm -f _temptree.nfo _temptree.dot
+rm -f _temptree.nfo _temptree.dot
 echo "Created tree image $2.png from input file $1"
